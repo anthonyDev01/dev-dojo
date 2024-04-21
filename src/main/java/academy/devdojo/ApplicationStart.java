c47786e2f9328ba3bd61f5076643be824579f9be
ANTHONY <anthonychukwudiDev@outlook.com>
2024-04-21 15:54:00 -0300

    estudando @Component, @Autowired, @SpringBootApplication

--- src/main/java/academy/devdojo/ApplicationStart.java
+++ src/main/java/academy/devdojo/ApplicationStart.java
@@ -1,11 +1,11 @@
-package academy.devdojo.start;
+package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "academy.devdojo")
+@ComponentScan
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
--- src/main/java/academy/devdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,16 +1,26 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Anime;
+import academy.devdojo.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+    private final DateUtil dateUtil;
     @RequestMapping("/list")
     public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return  List.of(new Anime("One Piece"), new Anime("Steins Gate"));
     }
 }
