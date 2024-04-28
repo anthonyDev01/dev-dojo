4c4c448fc2e93196404def75cefa308939847968
ANTHONY <anthonychukwudiDev@outlook.com>
2024-04-27 23:32:32 -0300

    criando conexao com mysql

--- src/main/java/academy/devdojo/spring/domain/Anime.java
+++ src/main/java/academy/devdojo/spring/domain/Anime.java
@@ -2,12 +2,19 @@ package academy.devdojo.spring.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
     private String name;
 
